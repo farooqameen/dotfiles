a944c9ca3aa6163c7182f3bd6bbedd2a7fff95b5
farooqameen <farooqameencs@gmail.com>
2025-08-29 01:10:14 +0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# update system
+sudo apt update && sudo apt upgrade -y
+
+# install gcc and developer tools
+sudo apt install build-essential -y 
+
+# install zsh
+# sudo apt install zsh -y
+
+# change shell
+# chsh -s $(which zsh)
