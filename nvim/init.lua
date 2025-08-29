2e7f1ebd3c7c2adf525a0dbe18f83c2e2a0a7d66
farooqameen <farooqameencs@gmail.com>
2025-08-29 14:53:44 +0300

    tabs, formatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,17 @@
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.wrap = false
+
 vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
 vim.o.swapfile = false
 vim.o.background = "dark"
 vim.o.timeoutlen = 800
 vim.g.mapleader = ' '
-vim.opt.clipboard = "unnamedplus"
+vim.o.clipboard = "unnamedplus"
+
 
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,11 +3,13 @@ local config = wezterm.config_builder()
 
 config.font_size = 14
 config.hide_tab_bar_if_only_one_tab = true
-config.keys = {
-		{
-				key = 'F11',
-				action = wezterm.action.ToggleFullScreen,
-		},
+
+config.keys = 
+{
+	{
+        key = 'F11',
+		action = wezterm.action.ToggleFullScreen,
+	},
 }
 
 return config
