a6d7b9aa5c39146f9314ab734f35e806a850e452
farooqameen <farooqameencs@gmail.com>
2025-08-30 04:46:01 +0300

    wezterm, sample font use

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,8 +1,13 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.font_size = 14
+config.font_size = 16
 config.hide_tab_bar_if_only_one_tab = true
+
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- config.font = wezterm.font('FiraCode Nerd Font Mono')
 
 config.keys = 
 {
